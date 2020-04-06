92cd4c76b1302f6fcd4de6db438cb2164c58944c
inhireev <inhireev@gmail.com>
2020-04-06 23:21:09 +0800

    список министерств

--- src/main/java/ru/sibdigital/addcovid/controller/DocRequestController.java
+++ src/main/java/ru/sibdigital/addcovid/controller/DocRequestController.java
@@ -4,13 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ru.sibdigital.addcovid.model.DepUser;
 import ru.sibdigital.addcovid.model.DocRequest;
 import ru.sibdigital.addcovid.repository.DepUserRepo;
 import ru.sibdigital.addcovid.repository.DocRequestRepo;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 public class DocRequestController {
@@ -24,11 +24,9 @@ public class DocRequestController {
     private static final Logger log = LoggerFactory.getLogger(DocRequestController.class);
 
     @GetMapping("/doc_requests")
-    public List<DocRequest> requests(Map<String, Object> model) {
+    public Optional<List<DocRequest>> requests(Map<String, Object> model) {
 
-        List<DocRequest> docRequests =  docRequestRepo.findAll();
-
-        //model.put("docRequests", docRequests);
+        Optional<List<DocRequest>> docRequests =  docRequestRepo.getAllByDepartmentId(3l);
 
         return docRequests;
     }
