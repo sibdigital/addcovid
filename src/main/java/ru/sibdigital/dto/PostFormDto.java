ef436650d9840cb574a271861040ad3db93d234a
SkaaRJ <vitya.filippow@yandex.ru>
2020-04-06 09:34:38 +0800

    Repositories added

---
+++ src/main/java/ru/sibdigital/dto/PostFormDto.java
@@ -0,0 +1,4 @@
+package ru.sibdigital.dto;
+
+public class PostFormDto {
+}
---
+++ src/main/java/ru/sibdigital/repository/DocRequestRepo.java
@@ -0,0 +1,6 @@
+package ru.sibdigital.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DocRequestRepo extends JpaRepository<DocRequestRepo, Long> {
+}
