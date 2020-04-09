f6442d7af087e085c769e8892cfb7eb95866c7b2
bulat <bulzhamur@gmail.com>
2020-04-09 14:40:10 +0800

    fix search

--- src/main/java/ru/sibdigital/addcovid/repository/DocRequestRepo.java
+++ src/main/java/ru/sibdigital/addcovid/repository/DocRequestRepo.java
@@ -20,13 +20,14 @@ public interface DocRequestRepo extends JpaRepository<DocRequest, Long> {
 
 //    @Query("SELECT dr FROM DocRequest dr WHERE  dr.department.id = :dep_id AND dr.statusReview = :status")
     @Query(value = "SELECT dr.* FROM doc_request dr, cls_organization org WHERE  dr.id_organization = org.id " +
-            " and dr.id_department = :dep_id AND dr.status_review = :status limit 100" , nativeQuery = true)
+            " and dr.id_department = :dep_id AND dr.status_review = :status ORDER BY dr.time_create ASC limit 100" , nativeQuery = true)
     Optional<List<DocRequest>> getAllByDepartmentId(@Param("dep_id")Long departmentId, @Param("status") Integer status);
+
 
     @Query(value = "SELECT dr.* FROM doc_request dr, cls_organization org WHERE  dr.id_organization = org.id " +
             " and dr.id_department = :dep_id AND dr.status_review = :status " +
-            " and (org.inn = :innOrName or lower(trim(org.name)) like %:innOrName%) limit 100" ,
-           // " ORDER BY dr.timeCreate DESC ",
+            " and (trim(org.inn) like %:innOrName% or lower(trim(org.name)) like %:innOrName%) " +
+            " ORDER BY dr.time_create ASC  limit 100",
             nativeQuery = true)
     Optional<List<DocRequest>> getFirst100RequestByDepartmentIdAndStatusAndInnOrName(@Param("dep_id")Long departmentId, @Param("status")
             Integer status, @Param("innOrName")String innOrName);
--- src/main/resources/static/js/utils/filter.js
+++ src/main/resources/static/js/utils/filter.js
@@ -6,6 +6,7 @@ define(function () {
                 id: 'search',
                 maxWidth: 300,
                 minWidth: 100,
+                tooltip: 'после ввода значения нажмите Enter',
                 placeholder: "Поиск по ИНН и названию",
                 on: {
                     // onTimedKeyPress: function () {
@@ -25,18 +26,21 @@ define(function () {
                     //     }
                     // }
 
-                    onTimedKeyPress: function () {
+                    //onTimedKeyPress: function () {
+                    onEnter: function () {
                         let text = this.getValue().toLowerCase();
                         let table = $$(tablename);
                         if (!text) {
-                            table.filter()
+                            //table.filter()
+                            table.clearAll()
+                            table.load(table.config.url)
                         }
                         else {
                             //url = 'list_requestByInnAndName/' + ID_DEPARTMENT + '/' + status + '/' + text
                             oldUrl = table.config.url
                             filterUrl = oldUrl + '/' +  text
                             table.clearAll();
-                            table.load (filterUrl)
+                            table.load(filterUrl)
                            // table.config.url =
                         }
                     }
