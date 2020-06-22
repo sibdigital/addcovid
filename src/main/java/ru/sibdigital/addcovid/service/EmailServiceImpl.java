df7cb2fc71d31804792ce8dd180af61439dbcec7
shedoev <shedoev@gmail.com>
2020-06-22 11:17:54 +0800

    Отправка писем асинхронным методом

--- src/main/java/ru/sibdigital/addcovid/service/EmailServiceImpl.java
+++ src/main/java/ru/sibdigital/addcovid/service/EmailServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +15,7 @@ public class EmailServiceImpl implements EmailService {
     @Autowired
     public JavaMailSender javaMailSender;
 
+    @Async
     public void sendSimpleMessage(String to, String subject, String text) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
