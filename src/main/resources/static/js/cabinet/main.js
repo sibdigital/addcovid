700f31d9d6057320ce20a7e4490bf9543c3193b9
shedoev <shedoev@gmail.com>
2020-07-06 11:28:42 +0800

    #606 - Смена пароля

--- src/main/resources/static/js/cabinet/main.js
+++ src/main/resources/static/js/cabinet/main.js
@@ -202,11 +202,73 @@ const requests = {
 }
 
 const settings = {
-    type: 'space',
-    rows: [
-        {
-        }
-    ]
+    view: 'scrollview',
+    scroll: 'xy',
+    body: {
+        type: 'space',
+        rows: [
+            {
+                view: 'form',
+                id: 'form_pass',
+                complexData: true,
+                elements: [
+                    {
+                        cols: [
+                            {
+                                rows: [
+                                    {
+                                        view: 'text',
+                                        id: 'new_pass',
+                                        name: 'password',
+                                        label: 'Новый пароль',
+                                        labelPosition: 'top',
+                                        type: 'password'
+                                    },
+                                    {
+                                        view: 'text',
+                                        id: 'retry_pass',
+                                        label: 'Подтвердите новый пароль',
+                                        labelPosition: 'top',
+                                        type: 'password',
+                                        on: {
+                                            onChange(newVal, oldVal) {
+                                                if (newVal === $$('new_pass').getValue()) {
+                                                    $$('save_pass').enable();
+                                                } else {
+                                                    $$('save_pass').disable();
+                                                }
+                                            }
+                                        }
+                                    },
+                                    {
+                                        view: 'button',
+                                        id: 'save_pass',
+                                        value: 'Применить',
+                                        click: () => {
+                                            webix.ajax().headers({'Content-Type': 'application/json'})
+                                                .post('/save_pass', $$('form_pass').getValues()).then(function (data) {
+                                                if (data.text() === 'Пароль обновлен') {
+                                                    $$('new_pass').setValue('');
+                                                    $$('retry_pass').setValue('');
+                                                    $$('save_pass').disable();
+                                                    webix.message(data.text(), 'success');
+                                                } else {
+                                                    webix.message(data.text(), 'error');
+                                                }
+                                            });
+                                        },
+                                        disabled: true
+                                    }
+                                ]
+                            },
+                            {},
+                            {}
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
 }
 
 webix.ready(function() {
