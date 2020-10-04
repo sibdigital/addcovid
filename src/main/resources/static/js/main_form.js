6d11839be4b292e09ec05b7a95ff108db4b33bad
alxtmf <89021632777@mail.ru>
2020-10-04 21:51:02 +0800

    Правка ссылок

--- src/main/resources/static/js/main_form.js
+++ src/main/resources/static/js/main_form.js
@@ -53,8 +53,8 @@ webix.ready(function() {
     webix.ajax('/cls_type_requests').then(function (data) {
         let typeRequests = data.json();
         let vtxt = `<span style="font-size:calc(1.1em  1vmin);text-align: center;">Подайте заявку в соответствии с вашим видом деятельности. </span><br/><br/>
-         <a style="font-size:calc(0.8em  1vmin); text-align: center;" href="${SUBDOMAIN_WORK}" >Общие основания</a><br/><br/>
-         <a style="font-size: calc(0.8em  1vmin); text-align: center;" href="${SUBDOMAIN_FORM}/upload/" >Общие основания (более 100 сотрудников)</a><br/><br/>
+         <a style="font-size:calc(0.8em  1vmin); text-align: center;" href="${SUBDOMAIN_WORK}/form" >Общие основания</a><br/><br/>
+         <a style="font-size: calc(0.8em  1vmin); text-align: center;" href="${SUBDOMAIN_WORK}/upload" >Общие основания (более 100 сотрудников)</a><br/><br/>
          <a style="font-size: calc(0.8em  1vmin); text-align: center;" href="${SUBDOMAIN_WORK}/barber" >Парикмахерские услуги</a><br/><br/>`;
 
         for(var  j = 0; j< typeRequests.length; j++){
