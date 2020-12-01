ab7799982b4d991aa99ee907cf1709827d9d956f
yovahu <poebot3r@gmail.com>
2020-12-01 22:28:32 +0800

    Контакты, добавлена форма

--- src/main/resources/static/js/cabinet/contacts.js
+++ src/main/resources/static/js/cabinet/contacts.js
@@ -5,121 +5,149 @@ const contacts = {
         type: 'space',
         rows: [
             {
-                view: "dataview",
-                id: "contact_grid",
-                css: "movies",
-                template: "<div class='webix_strong'>#contactPerson#</div> <span class='webix_light'>#contactValue#</span>",
-                url: "org_contacts",
-                xCount: 3,
-                type: {
-                    height: 60,
-                    width: "auto"
-                }
-            },
-            {
+                type: 'wide',
                 cols: [
                     {
-                        view: 'button',
-                        id: 'add_contact',
-                        css: 'webix_primary',
-                        minWidth: 150,
-                        maxWidth: 350,
-                        value: 'Добавить',
-                        click: () => {
-                            $$('contact_form').clear()
-                            contactWindow.show()
+                        view: "dataview",
+                        id: "contact_grid",
+                        css: 'contacts',
+                        height: userWindowHeight - 90,
+                        scroll: false,
+                        select: 1,
+                        url: "org_contacts",
+                        xCount: 3,
+                        type: {
+                            template: "<div class='overall'>" +
+                                "<div class='contactPerson'>#contactPerson#</div>" +
+                                "<div class='contactValue'>#contactValue#</div></div>",
+                            height: 95,
+                            width: "auto"
                         }
                     },
                     {
-                        view: 'button',
-                        id: 'del_contact',
-                        css: 'webix_primary',
-                        minWidth: 150,
-                        maxWidth: 350,
-                        value: 'Удалить',
-                        click: () => {
+                        gravity: 0.4,
+                        view: 'form',
+                        id: 'contact_form',
+                        complexData: true,
+                        rules: {
+                            "contactPerson": webix.rules.isNotEmpty,
+                            "type": webix.rules.isNotEmpty,
+                            "contactValue": webix.rules.isNotEmpty,
+                        },
+                        elements: [
+                            {gravity: 0.5},
+                            {
+                                view: 'text',
+                                name: 'contactPerson',
+                                label: 'ФИО',
+                                labelPosition: 'top',
+                                invalidMessage: "ФИО не может быть пустым"
+                            },
+                            {
+                                view: "combo",
+                                id: "type_combo",
+                                name: "type",
+                                label: 'Вид',
+                                labelPosition: 'top',
+                                value: "1",
+                                options: [
+                                    {id: 1, value: "Номер телефона"},
+                                    {id: 2, value: "Почтовый адрес"}
+                                ],
+                                on:{
+                                    onChange:() => {
+                                        if($$('type_combo').getValue() === 2){
+                                            $$('contactValueText').config.label = "Почта";
+                                            $$('contactValueText').config.placeholder = "sibdigital@mail.ru";
+                                            $$('contactValueText').refresh();
+                                        }
+                                        else if($$('type_combo').getValue() === 1)
+                                        {
+                                            $$('contactValueText').config.label = "Номер телефона";
+                                            $$('contactValueText').config.placeholder = "+7 (xxx) xxx-xx-xx";
+                                            $$('contactValueText').refresh()
+                                        }
+                                    }
+                                }
+                            },
+                            {
+                                view: 'text',
+                                id: 'contactValueText',
+                                name: 'contactValue',
+                                label: 'Номер телефона',
+                                placeholder: '+7 (xxx) xxx-xx-xx',
+                                labelPosition: 'top',
+                                invalidMessage: "Контакт не может быть пустым"
+                            },
+                            {
+                                cols: [
+                                    {
+                                        view: 'button',
+                                        id: 'add_contact',
+                                        css: 'webix_primary',
+                                        value: 'Добавить',
+                                        click: () => addContact()
+                                    },
+                                    {
+                                        view: 'button',
+                                        id: 'del_contact',
+                                        css: 'webix_primary',
+                                        value: 'Удалить',
+                                        click: () => deleteContact()
+                                    }
+                                ]
+                            },
+                            {}
+                        ]
 
-                        }
                     }
                 ]
-            }
+            },
+
 
         ],
     }
 }
 
-let contactWindow = webix.ui({
-    view: "window",
-    id: "updateModal",
-    minWidth: 200,
-    width: 550,
-    position: "center",
-    modal: true,
-    close: true,
-    head: "Редактирование контактов",
-    body: {
-        view: 'form',
-        id: 'contact_form',
-        complexData: true,
-        rules: {
-            "contactPerson": webix.rules.isNotEmpty,
-            "contactValue": webix.rules.isNotEmpty,
-        },
-        elements: [
-            {
-                view: 'text',
-                name: 'contactPerson',
-                label: 'contactPerson',
-                labelPosition: 'top',
-                invalidMessage: "contactPerson не может быть пустой"
-            },
-            {
-                view: 'text',
-                name: 'type',
-                label: 'type',
-                labelPosition: 'top',
-                invalidMessage: "Type не может быть пустым"
-            },
-            {
-                view: 'text',
-                name: 'contactValue',
-                label: 'contactValue',
-                labelPosition: 'top',
-                invalidMessage: "contactValue не может быть пустым"
-            },
-            {
-                view: 'button',
-                align: 'right',
-                css: 'webix_primary',
-                value: 'Добавить',
-                hotkey: "enter",
-                click: function () {
-                    let form = this.getParentView();
-                    let params = $$('contact_form').getValues()
-                    console.log(params)
-                    if (form.validate()) {
-                        webix.ajax()
-                            .headers({'Content-type': 'application/json'})
-                            .post('/save_contact', JSON.stringify(params))
-                            .then(function (data) {
-                                if (data !== null) {
-                                    $$('contact_form').clear()
-                                    if (params.id) {
-                                        webix.message('Контакт обновлен', 'success');
-                                    } else {
-                                        webix.message("Контакт добавлен", 'success');
-                                    }
-                                    $$('contact_grid').load('org_contacts');
-                                } else {
-                                    webix.message("Не удалось добавить контакт", 'error');
-                                }
-                            });
-                        $$('contact_form').clear()
-                        contactWindow.hide()
+function addContact() {
+    let form = $$('contact_form')
+    let params = form.getValues()
+    if (params["type"] === 1) params["type"] = 0;
+    else params["type"] = 1;
+    if (form.validate()) {
+        webix.ajax()
+            .headers({'Content-type': 'application/json'})
+            .post('/save_contact', JSON.stringify(params))
+            .then(function (data) {
+                if (data !== null) {
+                    if (params.id) {
+                        webix.message('Контакт обновлен', 'success');
+                    } else {
+                        webix.message("Контакт добавлен", 'success');
                     }
+                    $$('contact_grid').load('org_contacts');
+                } else {
+                    webix.message("Не удалось добавить контакт", 'error');
                 }
-            }
-        ]
-
+            });
+        form.clear()
     }
-});
+}
+
+function deleteContact() {
+    let params = $$('contact_grid').getSelectedItem()
+    webix.ajax()
+        .headers({'Content-type': 'application/json'})
+        .post('/delete_org_contact', JSON.stringify(params))
+        .then(function (data) {
+            if (data !== null) {
+                $$("contact_grid").remove($$("contact_grid").getSelectedId());
+                webix.message("Контакт удалён", 'success');
+                $$('contact_form').clear()
+                $$('contact_grid').load('org_contacts');
+            } else {
+                webix.message("Не удалось удалить контакт", 'error');
+            }
+        });
+    $$('contact_form').clear()
+}
