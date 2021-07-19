52891f4510762115e157cda01bd49c3cdee4d864
liudmila <gonchikova94@gmail.com>
2021-07-19 12:48:16 +0800

    headers with request ID

--- src/main/resources/static/js/cabinet/request_subsidy.js
+++ src/main/resources/static/js/cabinet/request_subsidy.js
@@ -248,6 +248,16 @@ function showRequestSubsidyViewForm(data) {
                             cols: [
                                 {
                                     view: 'text',
+                                    name: 'id',
+                                    autoheight: true,
+                                    // align: 'center',
+                                    width: 100,
+                                    label: 'ID заявки',
+                                    labelPosition: 'top',
+                                    readonly: true
+                                },
+                                {
+                                    view: 'text',
                                     name: 'subsidyName',
                                     autoheight: true,
                                     // align: 'center',
